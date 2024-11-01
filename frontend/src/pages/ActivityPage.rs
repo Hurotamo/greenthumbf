02e3dc25a7419f884aac0eab33c3f04f2a2ddeb7
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-01 14:17:34 +0800

    Update ActivityPage.rs

--- frontend/src/pages/ActivityPage.rs
+++ frontend/src/pages/ActivityPage.rs
@@ -1,1 +1,11 @@
+use yew::prelude::*;
+
+#[function_component(ActivityPage)]
+pub fn activity_page() -> Html {
+    html! {
+        <div class="activity-page">
+            <CameraSetup />
+        </div>
+    }
+}
 
