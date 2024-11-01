a7602e740c78f36eec02e1c96caa262550a2d88e
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-01 14:33:06 +0800

    Update token.rs

--- contracts/src/token.rs
+++ contracts/src/token.rs
@@ -1,1 +1,17 @@
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    msg,
+};
+
+pub fn mint_tokens(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    msg!("Minting tokens");
+    // Logic to mint tokens and assign them to an account
+    Ok(())
+}
 
