b9a7b9bd6089b825200d2803fc62a5312ccdb7e7
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-01 14:15:11 +0800

    Update CameraSetup.rs

--- frontend/src/components/CameraSetup.rs
+++ frontend/src/components/CameraSetup.rs
@@ -1,1 +1,11 @@
+use yew::prelude::*;
+
+#[function_component(CameraSetup)]
+pub fn camera_setup() -> Html {
+    html! {
+        <div class="camera-setup">
+            {"Camera setup will allow users to take pictures of their plants."}
+        </div>
+    }
+}
 
