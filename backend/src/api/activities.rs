1ddfaf00aa86e3e2ec6cafa8b89e5bad15d1d087
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-01 14:24:48 +0800

    Update activities.rs

--- backend/src/api/activities.rs
+++ backend/src/api/activities.rs
@@ -1,1 +1,7 @@
+use actix_web::{get, HttpResponse, Responder};
+
+#[get("/activities")]
+pub async fn activities_handler() -> impl Responder {
+    HttpResponse::Ok().json("Activities API is working!")
+}
 
