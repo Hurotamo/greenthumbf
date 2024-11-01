2d9feeab22fd5a992d51e20de331597087682c08
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-01 14:24:02 +0800

    Update mod.rs

--- backend/src/api/mod.rs
+++ backend/src/api/mod.rs
@@ -1,1 +1,13 @@
+use actix_web::web;
+
+mod wallet;
+mod subscription;
+mod activities;
+
+pub fn init_routes(cfg: &mut web::ServiceConfig) {
+    cfg
+        .service(wallet::wallet_handler)
+        .service(subscription::subscription_handler)
+        .service(activities::activities_handler);
+}
 
