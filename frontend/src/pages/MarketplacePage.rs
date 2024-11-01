5a494b49cfec5d6ecaeee64aed098013d1e0e296
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-01 14:17:21 +0800

    Update MarketplacePage.rs

--- frontend/src/pages/MarketplacePage.rs
+++ frontend/src/pages/MarketplacePage.rs
@@ -1,1 +1,11 @@
+use yew::prelude::*;
+
+#[function_component(MarketplacePage)]
+pub fn marketplace_page() -> Html {
+    html! {
+        <div class="marketplace-page">
+            <Marketplace />
+        </div>
+    }
+}
 
