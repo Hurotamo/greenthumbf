9b1d87ecc2aebd37a1e2b0398975ec25a59c7519
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-01 14:24:35 +0800

    Update subscription.rs

--- backend/src/api/subscription.rs
+++ backend/src/api/subscription.rs
@@ -1,1 +1,7 @@
+use actix_web::{get, HttpResponse, Responder};
+
+#[get("/subscription")]
+pub async fn subscription_handler() -> impl Responder {
+    HttpResponse::Ok().json("Subscription API is working!")
+}
 
