3686a23c29aa8f10384d3750a19dee0f97e24ed5
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-01 14:17:56 +0800

    Update App.rs

--- frontend/src/App.rs
+++ frontend/src/App.rs
@@ -1,1 +1,20 @@
+use yew::prelude::*;
+
+mod components;
+mod pages;
+
+use components::{Header};
+use pages::{HomePage, ProfilePage, MarketplacePage, ActivityPage};
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <>
+            <Header />
+            <main class="p-4">
+                <HomePage />
+            </main>
+        </>
+    }
+}
 
